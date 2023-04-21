cb2a5dbd22d8fff991e06242d47885c0610e6c17
MercyChebet <chebetm96@gmail.com>
2023-04-21 11:00:53 +0300

    Task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,25 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+// Add elements to the DOM
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
+$('body').append('<button id="btn">Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let conunt = 0;
+// Initialize counter
+let count = 0;
 
+// Function to update counter
 function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
-};
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
 
-$('button').on('click', _.debounce(updateCounter, 500));
+// Debounced function to bind to button click
+const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+
+// Bind function to button click
+$('#btn').click(debouncedUpdateCounter);
+
